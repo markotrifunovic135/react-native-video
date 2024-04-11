1f23d5c6aefba3c3f008c6788c805a98e155902f
Marko Trifunovic <trifunovicm@loni.ai>
2024-04-11 17:29:52 +0200

    revert fix

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -425,7 +425,7 @@ public class ReactExoplayerView extends FrameLayout implements
         });
 
         //Handling the playButton click event
-        ImageButton playButton = playerControlView.findViewById(com.google.android.exoplayer2.R.id.exo_play);
+        ImageButton playButton = playerControlView.findViewById(R.id.exo_play);
         playButton.setOnClickListener((View v) -> {
             if (player != null && player.getPlaybackState() == Player.STATE_ENDED) {
                 player.seekTo(0);
@@ -434,7 +434,7 @@ public class ReactExoplayerView extends FrameLayout implements
         });
 
         //Handling the pauseButton click event
-        ImageButton pauseButton = playerControlView.findViewById(com.google.android.exoplayer2.R.id.exo_pause);
+        ImageButton pauseButton = playerControlView.findViewById(R.id.exo_pause);
         pauseButton.setOnClickListener((View v) -> {
             setPausedModifier(true);
         });
